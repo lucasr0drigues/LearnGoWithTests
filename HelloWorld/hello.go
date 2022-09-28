819abcd1bc951c261e48cdd3a005b2e6bd62c188
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-09-27 21:49:52 -0300

    add language options

--- HelloWorld/hello.go
+++ HelloWorld/hello.go
@@ -2,15 +2,28 @@ package main
 
 import "fmt"
 
+const spanish = "Spanish"
+const french = "French"
 const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPreix = "Bonjour, "
 
 func main() {
-	fmt.Println(Hello("Lucas"))
+	fmt.Println(Hello("Lucas",""))
 }
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
+
+	if language == "Spanish"{
+		return spanishHelloPrefix + name
+	}
+
+	if language == "French"{
+		return frenchHelloPreix + name
+	}
+
 	return englishHelloPrefix + name
 }
