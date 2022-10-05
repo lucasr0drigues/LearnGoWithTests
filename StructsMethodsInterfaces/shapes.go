084faa35aaf97a9a4df33369c4879bc94e9e9d78
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-10-04 21:33:48 -0300

    fixing tests

--- StructsMethodsInterfaces/shapes.go
+++ StructsMethodsInterfaces/shapes.go
@@ -11,13 +11,13 @@ type Circle struct {
 	Radius float64
 }
 
-// func Perimeter(rectangle Rectangle) float64 {
-// 	return 2 * (rectangle.Width + rectangle.Height)
-// }
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Width + rectangle.Height)
+}
 
-// func Area(rectangle Rectangle) float64 {
-// 	return rectangle.Width * rectangle.Height
-// }
+func Area(rectangle Rectangle) float64 {
+	return rectangle.Width * rectangle.Height
+}
 
 func (r Rectangle) Area() float64 {
 	return r.Width * r.Height
--- StructsMethodsInterfaces/shapes_test.go
+++ StructsMethodsInterfaces/shapes_test.go
@@ -4,7 +4,7 @@ import "testing"
 
 func TestPerimeter(t *testing.T){
 	rectangle := Rectangle{10.0, 10.0}
-	got := rectangle.Area()
+	got := Perimeter(rectangle)
 	want := 40.0
 
 	if got != want {
