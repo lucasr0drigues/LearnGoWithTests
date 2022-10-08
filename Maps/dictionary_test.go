9b0c3c5113cd4b7d063fae76e6e35810d30de361
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-10-08 13:00:00 -0300

    add Add method

--- Maps/dictionary_test.go
+++ Maps/dictionary_test.go
@@ -13,17 +13,26 @@ func TestSearch(t *testing.T){
 	})
 
 	t.Run("unknown word", func(t *testing.T){
-		_, err := dictionary.Search("unknown")
-		want := "could not find the word you were looking for"
+		_, got := dictionary.Search("unknown")
 
-		if err == nil {
-			t.Fatal("expected to get an error.")
-		}
-
-		assertStrings(t, err.Error(), want)
+		assertError(t, got, ErrNotFound)
 	})
+}
 
+func TestAdd(t *testing.T){
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
 
+	want := "this is just a test"
+	got, err := dictionary.Search("test")
+
+	if err != nil {
+		t.Fatal("Should find added word: ", err)
+	}
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
 }
 
 func assertStrings(t testing.TB, got, want string){
@@ -31,5 +40,13 @@ func assertStrings(t testing.TB, got, want string){
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error){
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
 	}
 }
