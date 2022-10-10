3e87cf69f20d1e6e53b17e40f0193bd85e6f441d
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-10-09 21:53:10 -0300

    refactor TestAdd

--- Maps/dictionary_test.go
+++ Maps/dictionary_test.go
@@ -21,18 +21,12 @@ func TestSearch(t *testing.T){
 
 func TestAdd(t *testing.T){
 	dictionary := Dictionary{}
-	dictionary.Add("test", "this is just a test")
+	word := "test"
+	definition := "this is just a test"
 
-	want := "this is just a test"
-	got, err := dictionary.Search("test")
+	dictionary.Add(word, definition)
 
-	if err != nil {
-		t.Fatal("Should find added word: ", err)
-	}
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	assertDefinition(t, dictionary, word, definition)
 }
 
 func assertStrings(t testing.TB, got, want string){
@@ -48,5 +42,19 @@ func assertError(t testing.TB, got, want error){
 
 	if got != want {
 		t.Errorf("got error %q want %q", got, want)
+	}
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, definition string){
+	t.Helper()
+
+	got, err := dictionary.Search(word)
+
+	if err != nil {
+		t.Fatal("should find added word: ", err)
+	}
+
+	if definition != got {
+		t.Errorf("got %q want %q", got, definition)
 	}
 }
