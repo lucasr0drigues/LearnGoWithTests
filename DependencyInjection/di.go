fcf9fdaf908ec8f10c95258c81d2667ebf9f1224
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-10-12 21:11:05 -0300

    refactor main to use an http server

--- DependencyInjection/di.go
+++ DependencyInjection/di.go
@@ -3,13 +3,20 @@ package main
 import (
 	"fmt"
 	"io"
-	"os"
+	"log"
+	"net/http"
+	//"os"
 )
 
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
 }
 
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request){
+	Greet(w, "world")
+}
+
 func main(){
-	Greet(os.Stdout, "Elodie")
+	//Greet(os.Stdout, "Elodie")
+	log.Fatal(http.ListenAndServe(":5001", http.HandlerFunc(MyGreeterHandler)))
 }
