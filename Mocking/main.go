7ec8294260e3a2ccfafdf1fb8a317fca23dda64a
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-10-13 23:13:59 -0300

    refactor with consts and sleep

--- Mocking/main.go
+++ Mocking/main.go
@@ -4,16 +4,21 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
+
+const finalWorld = "Go!"
+const countdownStart = 3
 
 func main() {
 	Countdown(os.Stdout)
 }
 
 func Countdown(out io.Writer){ 
-	for i := 3; i > 0; i--{
+	for i := countdownStart; i > 0; i--{
 		fmt.Fprintln(out,i)
+		time.Sleep(1 * time.Second)
 	}
 
-	fmt.Fprintf(out, "Go!")
+	fmt.Fprintf(out, finalWorld)
 }
