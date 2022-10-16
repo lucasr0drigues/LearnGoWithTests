dfd4a37d482053aec01c56430fdef98597c10f07
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-10-15 21:25:28 -0300

    add SpyCountdownOperations

--- Mocking/main.go
+++ Mocking/main.go
@@ -20,6 +20,10 @@ type SpySleeper struct {
 	Calls int
 }
 
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
 func main() {
 	sleeper := &DefaultSleeper{}
 	Countdown(os.Stdout, sleeper)
@@ -41,3 +45,15 @@ func (s *SpySleeper) Sleep(){
 func (d *DefaultSleeper) Sleep(){
 	time.Sleep(1 * time.Second)
 }
+
+func (s *SpyCountdownOperations) Sleep(){
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error){
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
