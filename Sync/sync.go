2961c05e7e4ef2af5e3b3f0ccdfc159a56aec8f1
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-10-30 02:39:02 -0300

    add sync

---
+++ Sync/sync.go
@@ -0,0 +1,26 @@
+package main
+
+import "sync"
+
+// counter will increment a number.
+type Counter struct {
+	mu    sync.Mutex
+	value int
+}
+
+// NewCounter returns a new Counter.
+func NewCounter() *Counter {
+	return &Counter{}
+}
+
+// Increase the count.
+func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.value++
+}
+
+// Value returns the current count.
+func (c *Counter) Value() int {
+	return c.value
+}
