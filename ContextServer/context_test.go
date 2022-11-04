d7e5343ebbbcfa809e654a55584e64768d6c5c4a
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-11-03 22:43:02 -0300

    update context

--- ContextServer/context_test.go
+++ ContextServer/context_test.go
@@ -1,31 +1,18 @@
-package context
+package contextserver
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 	"time"
 )
 
-type SpyStore struct {
-	response  string
-	cancelled bool
-}
-
-func (s *SpyStore) Fetch() string {
-	time.Sleep(100 * time.Millisecond)
-	return s.response
-}
-
-func (s *SpyStore) Cancel() {
-	s.cancelled = true
-}
-
 func TestServer(t *testing.T) {
 	data := "hello, world"
 
 	t.Run("returns data from store", func(t *testing.T) {
-		store := &SpyStore{response: data, t: t}
+		store := &SpyStore{response: data}
 		svr := Server(store)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
@@ -36,12 +23,10 @@ func TestServer(t *testing.T) {
 		if response.Body.String() != data {
 			t.Errorf(`got "%s", want "%s"`, response.Body.String(), data)
 		}
-
-		store.assertWasNotCancelled()
 	})
 
 	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
-		store := &SpyStore{response: data, t: t}
+		store := &SpyStore{response: data}
 		svr := Server(store)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
@@ -50,10 +35,12 @@ func TestServer(t *testing.T) {
 		time.AfterFunc(5*time.Millisecond, cancel)
 		request = request.WithContext(cancellingCtx)
 
-		response := httptest.NewRecorder()
+		response := &SpyResponseWriter{}
 
 		svr.ServeHTTP(response, request)
 
-		store.assertWasCancelled()
+		if response.written {
+			t.Error("a response should not have been written")
+		}
 	})
 }
