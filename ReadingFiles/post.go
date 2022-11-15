dc196cf81b4c6294dfa53b1d474d53505633b83e
lucasr0drigues <lucas.a.rodrigues@outlook.com>
2022-11-15 00:24:53 -0300

    refactor and add readBody func

--- ReadingFiles/post.go
+++ ReadingFiles/post.go
@@ -21,32 +21,27 @@ const (
 	tagsSeparator        = "Tags: "
 )
 
-func newPost(postFile io.Reader) (Post, error) {
-	scanner := bufio.NewScanner(postFile)
+func newPost(postBody io.Reader) (Post, error) {
+	scanner := bufio.NewScanner(postBody)
 
 	readMetaLine := func(tagName string) string {
 		scanner.Scan()
 		return strings.TrimPrefix(scanner.Text(), tagName)
 	}
 
-	title := readMetaLine(titleSeparator)
-	description := readMetaLine(descriptionSeparator)
-	tags := strings.Split(readMetaLine(tagsSeparator), ", ")
+	return Post{
+		Title:       readMetaLine(titleSeparator),
+		Description: readMetaLine(descriptionSeparator),
+		Tags:        strings.Split(readMetaLine(tagsSeparator), ", "),
+		Body:        readBody(scanner),
+	}, nil
+}
 
-	scanner.Scan()
-
+func readBody(scanner *bufio.Scanner) string {
+	scanner.Scan() // ignore a line
 	buf := bytes.Buffer{}
-
 	for scanner.Scan() {
 		fmt.Fprintln(&buf, scanner.Text())
 	}
-
-	body := strings.TrimSuffix(buf.String(), "\n")
-
-	return Post{
-		Title:       title,
-		Description: description,
-		Tags:        tags,
-		Body:        body,
-	}, nil
+	return strings.TrimSuffix(buf.String(), "\n")
 }
